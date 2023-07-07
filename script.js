81f689b449df7733031c1b3142c321d70de9f6a1
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-07-08 01:20:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	const keyValue = sampleObject[key];
+	if(keyValue != undefined){
+		return true;
+	}
+	return false;
 }
 
 // Do not change the code below
